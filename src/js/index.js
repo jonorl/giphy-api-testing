c8f3e9f142004ca210ab288b5aa6b86a50301afe
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-12 10:00:05 +0300

    input search bar working fine, need to add a catch now

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,39 @@ import "../css/style.css";
 
 // Global variables
 
-const img = document.querySelector('img');
-const btn = document.querySelector('#new-cat-button')
+const img = document.querySelector("img");
+const randomBtn = document.querySelector("#random-cat-button");
+const searchBtn = document.querySelector("#search-cat-button");
+const input = document.querySelector("input");
 
 // Event listeners
 
 fetchCatGif();
-btn.addEventListener('click', fetchCatGif);
+randomBtn.addEventListener("click", fetchCatGif);
+searchBtn.addEventListener("click", () => {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=pOslsruwcUp3D8BDLDebOYkF0yvwfpDf&s=${input.value}`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+});
 
 // Aux Functions
 
-function fetchCatGif(){
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=pOslsruwcUp3D8BDLDebOYkF0yvwfpDf&s=cats', {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
+function fetchCatGif() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=pOslsruwcUp3D8BDLDebOYkF0yvwfpDf&s=cats",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
 }
