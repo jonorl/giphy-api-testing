45a6e3e6c0a36a09394e23b0e22d726029e2b71e
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-12 09:36:56 +0300

    Added button to refresh the image without reloading

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,17 @@ import "../css/style.css";
 
 // Global variables
 
+const img = document.querySelector('img');
+const btn = document.querySelector('#new-cat-button')
+
+// Event listeners
+
+fetchCatGif();
+btn.addEventListener('click', fetchCatGif);
+
 // Aux Functions
 
-const img = document.querySelector('img');
+function fetchCatGif(){
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=pOslsruwcUp3D8BDLDebOYkF0yvwfpDf&s=cats', {mode: 'cors'})
   .then(function(response) {
     return response.json();
@@ -12,3 +20,4 @@ fetch('https://api.giphy.com/v1/gifs/translate?api_key=pOslsruwcUp3D8BDLDebOYkF0
   .then(function(response) {
     img.src = response.data.images.original.url;
   });
+}
